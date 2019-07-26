1d8a1b449f98a8edb92ca5bb77d7a63891b09602
BrendaMwiza <brendabrizy@gmail.com>
2019-07-26 12:26:49 +0200

    modify images

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".img1").click(function(){
+      $(".par1").show();
+      $(".img1").hide(); 
+    });
+    $(".par1").click(function(){
+        $(".img1").show();
+        $(".par1").hide(); 
+    });
+
+    $(".img2").click(function(){
+        $(".par2").show();
+        $(".img2").hide(); 
+      });
+      $(".par2").click(function(){
+          $(".img2").show();
+          $(".par2").hide(); 
+      });
+
+      $(".img3").click(function(){
+        $(".par3").show();
+        $(".img3").hide(); 
+      });
+      $(".par3").click(function(){
+          $(".img3").show();
+          $(".par3").hide(); 
+      });
+});
